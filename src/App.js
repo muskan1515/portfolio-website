7dd741ff1afa730bd71e8ea9b9f4bb3bd3e32563
muskan1515 <muskankushwah85@gmail.com>
2025-08-23 02:27:20 +0530

    added all th important details about my bio

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import "./App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      resumeData: {},
+      sharedData: {},
+    };
+  }
+
+  componentDidMount() {
+    this.loadSharedData();
+    this.loadResumeFromPath("res_primaryLanguage.json"); // Always English
+    document.documentElement.lang = "en"; // Force English
+  }
+
+  loadResumeFromPath(path) {
+    $.ajax({
+      url: path,
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert(err);
+      },
+    });
+  }
+
+  loadSharedData() {
+    $.ajax({
+      url: `portfolio_shared_data.json`,
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ sharedData: data });
+        document.title = `${data.basic_info.name}`;
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert(err);
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header sharedData={this.state.sharedData.basic_info} />
+
+        {/* You can keep or remove the UK flag */}
+        <div className="col-md-12 mx-auto text-center language">
+          <span
+            className="iconify language-icon"
+            data-icon="twemoji-flag-for-flag-united-kingdom"
+            data-inline="false"
+          ></span>
+        </div>
+
+        <About
+          resumeBasicInfo={this.state.resumeData.basic_info}
+          sharedBasicInfo={this.state.sharedData.basic_info}
+        />
+        <Projects
+          resumeProjects={this.state.resumeData.projects}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Skills
+          sharedSkills={this.state.sharedData.skills}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Experience
+          resumeExperience={this.state.resumeData.experience}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
+      </div>
+    );
+  }
+}
+
+export default App;
